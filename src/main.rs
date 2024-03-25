1185507f1cd541177f3fe6267316e1ada658481d
cs97 <chrswr97@gmail.com>
2024-03-25 16:07:09 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,17 @@
 use actix_web::{get, App, HttpResponse, HttpServer, Responder};
 use actix_files as fs;
 
+
+// login
+#[put("/login")]
+async fn login(req: &HttpRequest) -> impl Responder {
+	let txt = req.headers().get("content-type").expect("nope").to_str().ok();
+	println!("{}", txt);
+
+    HttpResponse::Ok()
+}
+
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
