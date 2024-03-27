125ee8010c2b077e4a4b90b189ba4f7f642db67f
cs97 <chrswr97@gmail.com>
2024-03-27 20:46:42 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,10 @@ async fn test(request: HttpRequest) -> impl Responder {
 	let basic_auth: &str = basic_auth_header.unwrap().to_str().unwrap();
 	println!("{}", basic_auth);
 
+	if "eyJ1c2VybmFtZSI6Imtla3ciLCJwYXNzd29yZCI6Im5vcGUifQ==" != basic_auth {
+		HttpResponse::Ok().body("false")
+	}
+
 	HttpResponse::Ok().body("true")
 }
 
