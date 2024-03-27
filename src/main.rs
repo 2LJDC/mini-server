2dddcb7d368bc0f2a29a4b0504a34cefa86aa8f5
cs97 <chrswr97@gmail.com>
2024-03-27 03:15:53 +0100

    .

--- src/main.rs
+++ src/main.rs
@@ -43,7 +43,7 @@ async fn printdata(request: HttpRequest) -> impl Responder {
 	//let data = match sqlx::query("SELECT * FROM kunde;")
 	//	.execute(&pool).await{
 	let data = sqlx::query!("select (1) as id, 'Herp Derpinson' as name")
-		.fetch_one(&mut pool).await {}
+		.fetch_one(&mut pool).await {
 			Ok(data) => data,
 			Err(e) => return HttpResponse::Ok().body("nono"),
 		};
