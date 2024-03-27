0525b4499fb0336b03f46c5114c08766ca9c36d3
cs97 <chrswr97@gmail.com>
2024-03-27 04:15:42 +0100

    .

--- src/main.rs
+++ src/main.rs
@@ -61,15 +61,14 @@ async fn printdata(request: HttpRequest) -> impl Responder {
 		};*/
 
 	//let data: Vec<String> = match sqlx::query_scalar("SELECT * FROM kunde;")
-	let data = match sqlx::query_as::<_, MSG>("SELECT * FROM kunde;")
-
+	let data = match sqlx::query_scalar("SELECT * FROM kunde;")
 		.fetch_one(&pool).await{
 			Ok(data) => data,
 			Err(e) => return HttpResponse::Ok().body("nono dont..."),
 		};
 
 	
-	let datastr = format!("{:?}", data);
+	let datastr = format!("{:?}", data.0);
 
 	
 	HttpResponse::Ok().body(datastr)
